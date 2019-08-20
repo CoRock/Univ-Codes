5fb37d3cf7fd907a70210f10075ad9584f0f91cd
corock <hoongineer@gmail.com>
2019-08-20 22:52:04 +0900

    Add linked list examples

---
+++ c-sharp/Lec181206.LinkedList/Lec181206.LinkedList/ListNode.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lec181206.LinkedList
+{
+    class ListNode
+    {
+        int data;
+        ListNode next;
+
+        public ListNode()
+        {
+            data = 0;
+            next = null;
+        }
+
+        public ListNode(int x)
+        {
+            data = x;
+            next = null;
+        }
+
+        public ListNode(int x, ListNode l)
+        {
+            data = x;
+            next = l;
+        }
+
+        public int getData()
+        {
+            return data;
+        }
+
+        public void setData(int x)
+        {
+            data = x;
+        }
+
+        public ListNode getNext()
+        {
+            return next;
+        }
+
+        public void setNext(ListNode next)
+        {
+            this.next = next;
+        }
+    }
+}
